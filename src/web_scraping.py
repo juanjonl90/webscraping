6837f3c74b50ff0f522a112c3f5e211034b04f16
juanjonl90 <juanjonl90@gmail.com>
2024-01-13 10:41:03 +0100

    creacion de webscraping.py

--- src/web_scraping.py
+++ src/web_scraping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 
 # URL de de telemadrid
 url = 'https://www.telemadrid.es/'
@@ -21,21 +22,61 @@ try:
                 noticias = soup.find_all('article', class_='card-news')
                 if noticias:
                     #print(noticias)
+                    lista_categorias = []
                     for articulo in noticias:
                         #print(articulo)
                         try:
                             titulo = articulo.find('a', class_='oop-link').text.strip()
                             url_noticia = articulo.find('a', class_='opp-link')['href']
+                            print(url_noticia)
                             lista_url_noticia = url_noticia.split('/')
-                            print(lista_url_noticia)
+                            if lista_url_noticia[1] != '':
+                                categoria = lista_url_noticia[1]
+                            else:
+                                categoria = lista_url_noticia[3]
+                            lista_categorias.append(categoria)
+                            lista_fecha = url_noticia.split('--')
+                            fecha = lista_fecha[1].replace('.html','')
+                            fecha_caracteres = lista_fecha[1].replace('.html', '')
+                            # print(fecha_caracteres)
+                            # print(fecha_caracteres[0:4])
+                            # print(fecha_caracteres[4:6])
+                            # print(fecha_caracteres[6:8])
+                            # print(fecha_caracteres[8:10])
+                            # print(fecha_caracteres[10:12])
+                            # print(fecha_caracteres[12:14])
+                            fecha = datetime(int(fecha_caracteres[0:4]), int(fecha_caracteres[4:6]),
+                                             int(fecha_caracteres[6:8]), int(fecha_caracteres[8:10]),
+                                             int(fecha_caracteres[10:12]), int(fecha_caracteres[12:14]))
+                            fecha = fecha.strftime("%Y/%m/%d %H:%M:%S")
+                            print(fecha)
+                            print("hola")
                         except:
                             try:
                                 titulo = articulo.find('a', class_='lnk').text.strip()
                                 url_noticia = articulo.find('a', class_='lnk')['href']
                                 lista_url_noticia = url_noticia.split('/')
-                                print(lista_url_noticia)
+                                if lista_url_noticia[1] != '':
+                                    categoria = lista_url_noticia[1]
+                                else:
+                                    categoria = lista_url_noticia[3]
+                                lista_categorias.append(categoria)
+                                lista_fecha = url_noticia.split('--')
+                                fecha_caracteres = lista_fecha[1].replace('.html', '')
+                                #print(fecha_caracteres)
+                                #print(fecha_caracteres[0:4])
+                                #print(fecha_caracteres[4:6])
+                                #print(fecha_caracteres[6:8])
+                                #print(fecha_caracteres[8:10])
+                                #print(fecha_caracteres[10:12])
+                                #print(fecha_caracteres[12:14])
+                                fecha = datetime(int(fecha_caracteres[0:4]), int(fecha_caracteres[4:6]), int(fecha_caracteres[6:8]),int(fecha_caracteres[8:10]),int(fecha_caracteres[10:12]),int(fecha_caracteres[12:14]))
+                                fecha = fecha.strftime("%Y/%m/%d %H:%M:%S")
                             except:
                                 pass
+                    #print(lista_categorias)
+                    conjunto_categorias = set(lista_categorias)
+                    print(conjunto_categorias)
                 else:
                     print(f"Error La pagina {url} no contiene noticias")
             except:
@@ -43,9 +84,6 @@ try:
         except:
             print(f"ERROR: no se pudo convertir la pagina a codigo html")
     else:
-        print(f"Error al obtener la página. Código de estado: {respuesta.status_code}")
+        print(f"Error al obtener la página web. Código de estado: {respuesta.status_code}")
 except:
-    print(f"ERrOR: No se PUEDE abrir la pagina {url} o existe un error al procesarla")
-
-
-
+    print(f"ERROR: No se puede abrir la web pagina {url} o existe un error al procesarla")
